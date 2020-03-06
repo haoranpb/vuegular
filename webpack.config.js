8364e3fd92ccd603facb68f8f2ea3b0e0c6de86e
ludanxer <ludanxer@gmail.com>
2020-03-06 18:11:08 +0800

    feat: hello babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,18 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
