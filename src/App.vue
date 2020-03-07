f12795c0a2af9bbe60054ace809a005e192af66c
ludanxer <ludanxer@gmail.com>
2020-03-07 19:04:32 +0800

    feat: footer and header components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <the-header />
     <h1>Hello Vuejs!</h1>
+    <the-footer />
   </div>
 </template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>This is the header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'the-header',
+};
+</script>
