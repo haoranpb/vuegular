8c1ab93ed4ee8b9706e47997ff6d06d7aa575a00
ludanxer <ludanxer@gmail.com>
2020-03-07 23:46:33 +0800

    fix: correctly set production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  mode: process.env.production ? 'production' : 'development',
+  mode: isProd ? 'production' : 'development',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: process.env.production ? false : 'inline-source-map',
+  devtool: isProd ? false : 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
