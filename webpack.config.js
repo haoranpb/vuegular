9d374cc4ad212addd7f6954d36fdf59b27d01884
ludanxer <ludanxer@gmail.com>
2020-03-07 18:27:46 +0800

    feat: development mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+  mode: process.env.production ? 'production' : 'development',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: process.env.production ? false : 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
