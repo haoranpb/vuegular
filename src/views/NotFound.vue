7a59ca377f6e80c0050e5d5372dfee31456a2001
ludanxer <ludanxer@gmail.com>
2020-03-09 17:09:10 +0800

    feat: style 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="not-found">
-    <h1>Oops, there is nothing here. 404</h1>
+    <h2>404 Oops, there is nothing here.</h2>
     <router-link to="/">Take Me Home</router-link>
   </div>
 </template>
@@ -10,3 +10,18 @@ export default {
   name: 'not-found',
 }
 </script>
+
+<style scoped>
+#not-found {
+  height: calc(100vh - 4rem - 5rem);
+  padding-top: 10%;
+}
+#not-found a {
+  font-size: 1.1rem;
+  font-weight: 600;
+  color: var(--primary-color);
+}
+#not-found h2 {
+  margin-bottom: 1rem;
+}
+</style>
