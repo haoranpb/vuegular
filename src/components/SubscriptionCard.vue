2957dd9bed3dfa2d14b29656eb2111dca2517c0a
ludanxer <ludanxer@gmail.com>
2020-03-10 09:39:19 +0800

    feat: init subscription card

---
+++ src/components/SubscriptionCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="subscription-card">
+    <h3 class="card-title">Title</h3>
+    <p class="card-note">This is some notes</p>
+    <h4 class="card-price">$100 / month</h4>
+    <p class="card-date">Due Date</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'subscription-card',
+}
+</script>
+
+<style lang="scss" scope>
+.subscription-card {
+  color: white;
+  height: 10rem;
+  width: 20rem;
+  background-color: $primary-color;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="home">
     <h1>This is home page</h1>
+    <subscription-card></subscription-card>
   </div>
 </template>
 
 <script>
+import SubscriptionCard from '../components/SubscriptionCard.vue'
+
 export default {
   name: 'home',
+  components: { SubscriptionCard },
 }
 </script>
