8638c253f3339621e2dd3e360eb5c1b969c3150b
ludanxer <ludanxer@gmail.com>
2020-03-14 17:39:47 +0800

    feat: about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,45 @@
 <template>
   <div id="about">
-    <h1>This is about page</h1>
+    <h3>What is Vuegular?</h3>
+    <p>
+      Usually, I love using
+      <external-link href="https://cli.vuejs.org/"> vue-cli</external-link> to
+      quickly start a project 🚀 and focus on the website I want to build, it's
+      an amazing tool so I don't have to worry about staffs like webpack.
+      However, things have changed for me, now I want to dig a little deeper
+      into frontend development and try to better understand all those utilities
+      🧐.
+    </p>
+    <p>
+      So here at Vuegular, I'll try to build a simple web application with Vuejs
+      <b>from scratch</b>. And I hope I could finish this and benefit from it.
+      If you are interested in how I build Vuegular step by step, check out the
+      <external-link href="https://github.com/ludanxer/vuegular/commits/master"
+        >commit history</external-link
+      >!
+    </p>
   </div>
 </template>
 
 <script>
+import ExternalLink from '../components/ExternalLink.vue'
+
 export default {
   name: 'about',
+  components: { ExternalLink },
 }
 </script>
+
+<style lang="scss" scoped>
+#about {
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+  p {
+    margin-top: 3%;
+  }
+  .external-link {
+    color: $primary-color;
+    font-weight: 500;
+  }
+}
+</style>
