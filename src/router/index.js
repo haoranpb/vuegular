f6b91730a428a7d67e82d3868cce599ec568fda6
ludanxer <ludanxer@gmail.com>
2020-03-15 14:02:59 +0800

    perf: dynamic import 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
 const About = () => import('../views/About.vue')
-import NotFound from '../views/NotFound.vue'
+const NotFound = () => import('../views/NotFound.vue')
 
 Vue.use(Router)
 
