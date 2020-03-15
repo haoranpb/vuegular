c259ec159d5d4eaf83ba552821044c9b35db0c7d
ludanxer <ludanxer@gmail.com>
2020-03-15 11:37:33 +0800

    perf: dynamic import about page
    babel and eslint need to be configed to get things working

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+const About = () => import('../views/About.vue')
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(Router)
--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,7 @@ module.exports = env => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-syntax-dynamic-import'],
             },
           },
         },
